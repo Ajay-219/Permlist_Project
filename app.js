7b3422c68e2c678b1ea95d0ec7cee249aea46196
Ajay Jeripothula <ajayjeripothula219@gmail.com>
2026-02-07 00:56:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "Permalist",
-  password: "ajay2004",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 db.connect();
 
